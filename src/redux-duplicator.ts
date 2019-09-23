39d293189dbed2015c09271503acf5c9893dbf03
tkow <t.kusama0613@outlook.jp>
2019-09-23 23:19:01 +0900

    [chore] dependency fix

--- src/redux-duplicator.ts
+++ src/redux-duplicator.ts
@@ -1,6 +1,48 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
-export default class DummyClass {
+// import "core-js/fn/array.find"
+// ...
 
+export const recreateActionTypes = <T extends { [key: string]: string }>(
+  actionTypes: T,
+  prefix: string
+) => {
+  return Object.keys(actionTypes).reduce((data, key) => {
+    return {
+      ...data,
+      [key]: `${prefix}${actionTypes[key]}`
+    }
+  }, {}) as { [key in keyof T]: string }
+}
+
+export const recreateActionCreators = <
+  ActionCreators extends {
+    [key: string]: <Args, Action extends { type: string }>(...args: Args[]) => Action
+  }
+>(
+  actionCreators: ActionCreators,
+  prefix: string
+) => {
+  return Object.keys(actionCreators).reduce((records, key) => {
+    return {
+      ...records,
+      [key]: <T, Action extends { type: string }>(...args: T[]): Action => {
+        const action = actionCreators[key](...args)
+        return {
+          ...action,
+          type: `${prefix}${action.type}`
+        } as Action
+      }
+    }
+  }, {})
+}
+
+export const reuseReducer = <Reducer>(reducer: Reducer, prefix: string): Reducer => {
+  const matchPattern = new RegExp(`^${prefix}`)
+  return function(state, action) {
+    if (matchPattern.test(action.type)) {
+      return reducer(state, action)
+    } else {
+      return state
+    }
+  } as Reducer
 }
