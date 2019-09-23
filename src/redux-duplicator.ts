c3bc2e3066ca9f8bb0d51a84fde05ed6fc135428
tkow <t.kusama0613@outlook.jp>
2019-09-23 23:39:52 +0900

    [chore] add libraries

--- src/redux-duplicator.ts
+++ src/redux-duplicator.ts
@@ -1,6 +1,4 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-// import "core-js/fn/array.find"
-// ...
+import { Reducer, Action } from 'redux'
 
 export const recreateActionTypes = <T extends { [key: string]: string }>(
   actionTypes: T,
@@ -36,13 +34,47 @@ export const recreateActionCreators = <
   }, {})
 }
 
-export const reuseReducer = <Reducer>(reducer: Reducer, prefix: string): Reducer => {
+export const reuseReducer = <S, A extends Action<any>>(
+  reducer: Reducer<S, A>,
+  prefix: string
+): Reducer<S, A> => {
   const matchPattern = new RegExp(`^${prefix}`)
-  return function(state, action) {
-    if (matchPattern.test(action.type)) {
-      return reducer(state, action)
+  return function(state: S, action: A) {
+    const matcher = matchPattern.exec(action.type)
+    if (matcher) {
+      const originalAction = {
+        ...action,
+        type: matcher[1]
+      }
+      return reducer(state, originalAction)
     } else {
       return state
     }
-  } as Reducer
+  } as Reducer<S, A>
+}
+
+export default function duplicateRedux<
+  ActionTypes extends { [key: string]: string },
+  ActionCreators extends {
+    [key: string]: <Args, Action extends { type: string }>(...args: Args[]) => Action
+  },
+  S,
+  A extends Action<any>
+>(
+  prefix: string,
+  {
+    actionTypes,
+    actionCreators,
+    reducer
+  }: {
+    actionTypes: ActionTypes
+    actionCreators: ActionCreators
+    reducer: Reducer<S, A>
+  }
+) {
+  return {
+    reducer: reuseReducer(reducer, prefix),
+    actionTypes: recreateActionTypes(actionTypes, prefix),
+    actionCreators: recreateActionCreators(actionCreators, prefix)
+  }
 }
